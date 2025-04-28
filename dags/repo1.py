2289495eaa20282feda314dfed81404318285916
Brennan Smith <brennan.smith@astronomer.io>
2025-04-28 13:46:20 -0400

    Testing import module varieties.

--- dags/repo1.py
+++ dags/repo1.py
@@ -1,15 +1,13 @@
 import datetime 
-
+import importlib
 from airflow.decorators import dag, task
 from airflow.operators.empty import EmptyOperator 
-
-from scripts.script1 import MyClass
 
 @dag(start_date=datetime.datetime(2021, 1, 1), schedule='@daily')
 def repo1_dag():
     @task
     def printer():
-        a = MyClass()
-        print(MyClass.x)
+        a = importlib.import_module("gitsub-repo1.scripts.script1.MyClass")
+        print(a.x)
 
 repo1_dag()
