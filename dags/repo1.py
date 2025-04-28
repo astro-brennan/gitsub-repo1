7dd3b0e7bcd034a0e0d3a5c6abd011453d61caa8
Brennan Smith <brennan.smith@astronomer.io>
2025-04-28 14:00:47 -0400

    Fixed task dependency

--- dags/repo1.py
+++ dags/repo1.py
@@ -1,13 +1,14 @@
 import datetime 
 import importlib
-from airflow.decorators import dag, task
-from airflow.operators.empty import EmptyOperator 
 
-@dag(start_date=datetime.datetime(2021, 1, 1), schedule='@daily')
+from airflow.decorators import dag, task
+
+@dag(start_date=datetime.datetime(2025, 1, 1), schedule='@once')
 def repo1_dag():
     @task
     def printer():
         a = importlib.import_module("gitsub-repo1.scripts.script1.MyClass")
         print(a.x)
 
+    printer()
 repo1_dag()
