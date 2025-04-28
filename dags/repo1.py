d4e54376ebec92d11cda699866dbc75d118b724b
Brennan Smith <brennan.smith@astronomer.io>
2025-04-28 14:14:35 -0400

    Fixing

--- dags/repo1.py
+++ dags/repo1.py
@@ -2,13 +2,14 @@ import datetime
 import importlib
 
 from airflow.decorators import dag, task
+from scripts.script1 import MyClass
 
 @dag(start_date=datetime.datetime(2025, 1, 1), schedule='@once')
 def repo1_dag():
     @task
     def printer():
-        a = importlib.import_module("gitsub-repo1.scripts.script1.MyClass")
-        print(a.x)
+        a = importlib.import_module("scripts.script1.MyClass")
+        print(MyClass.x)
 
     printer()
 repo1_dag()
